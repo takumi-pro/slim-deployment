bb0d5476a727f31e11b7536417b5ed27ff6c1b1e
takumi-pro <takumidiary.0927@gmail.com>
2023-06-04 22:06:13 +0900

    PHPUnit導入

---
+++ test/helloTest.php
@@ -0,0 +1,14 @@
+<?php
+
+require('vendor/autoload.php');
+
+use App\Controllers\HelloController;
+use PHPUnit\Framework\TestCase;
+
+class HelloTest extends TestCase {
+    public function testHello(): void {
+        $hello = new HelloController();
+        $result = $hello->hello();
+        $this->assertEquals('Hello', $result);
+    }
+}
