9b78046b15dee57c85503846f723bf8ae9e06c6e
takumi-pro <takumidiary.0927@gmail.com>
2023-06-04 22:32:09 +0900

    test修正

--- test/helloTest.php
+++ test/helloTest.php
@@ -5,8 +5,10 @@ require('vendor/autoload.php');
 use App\Controller\HelloController;
 use PHPUnit\Framework\TestCase;
 
-class HelloTest extends TestCase {
-    public function testHello(): void {
+class HelloTest extends TestCase
+{
+    public function testHello(): void
+    {
         $hello = new HelloController();
         $result = $hello->hello();
         $this->assertEquals('Hello', $result);
